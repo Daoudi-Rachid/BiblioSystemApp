b6b170d9b17d9257cb903585ff74b2455a4dad72
Daoudi-Rachid <daoudi.rachid@free.fr>
2021-02-23 14:31:59 +0100

    deuxieme commit

--- src/main/java/fr/groupe3/model/Emprunt.java
+++ src/main/java/fr/groupe3/model/Emprunt.java
@@ -1,38 +1,27 @@
 package fr.groupe3.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
-public class Bibliothecaire {
-
+public class Emprunt {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
 	@NotNull
+	private Date DateEmprunt;
+	@NotNull
+	private Date DateRetour;
 	@OneToOne
-	private Utilisateur utilisateur;
-
-	public Bibliothecaire() {}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public Utilisateur getUtilisateur() {
-		return utilisateur;
-	}
-
-	public void setUtilisateur(Utilisateur utilisateur) {
-		this.utilisateur = utilisateur;
-	}
+	@JoinColumn(name="livre_id")
+	private Livre livre;
+	
 }
